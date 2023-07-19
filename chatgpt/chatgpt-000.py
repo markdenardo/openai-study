def4c7143ebb2aed79712f959f4dd356c90698b0
MDN <mark.denardo@gmail.com>
2023-07-19 15:04:02 -0400

    Update chatgpt-000.py

--- chatgpt/chatgpt-000.py
+++ chatgpt/chatgpt-000.py
@@ -3,7 +3,7 @@ import sys
 sys.path
 sys.path.append('/usr/local/lib/python3.9/site-packages')
 
-openai.api_key = ""
+openai.api_key = "API_KEY"
 
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
                                           {"role": "user", "content": "write a short story about capybara making friends"}])
